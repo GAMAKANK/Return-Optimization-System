1065404348a0c75d83d8c0f325eadf2fc4a3ed3a
deepak <deepakk.ug23.cs@nitp.ac.in>
2025-07-09 10:31:24 +0530

    controllers and routes are added

---
+++ Infra-Orchestration/api_services/models/model.return.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+const returnSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product', // Reference to the Product model,
+        required: true,
+    },
+    reason: {
+        type: String,
+        trim: true
+    },
+    conditon: { 
+        type: String,
+        required: true,
+    },
+    purchaseDate: {
+        type: Number,
+        required: true,
+    },
+    customerType: {
+        type: String,
+        required: true,
+    },
+    predictedAction: {
+        type: String,
+        trim: true
+    },
+    confidence: {
+        type: Number,
+        required: true,
+    },
+    batchId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Batch',
+        },
+    status:{
+        type: String,
+        enum: ['pending', 'approved', 'rejected'],
+        default: 'pending',
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+}, );
+
+
+model.exports = mongoose.model('Return', returnSchema);
